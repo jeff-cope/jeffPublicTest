d8a9ff102cc6d2cc7252a44f8135650c945db700
jeff-cope <85047654+jeff-cope@users.noreply.github.com>
2023-10-11 20:44:20 -0400

    Create SqrtException.java

---
+++ SqrtException.java
@@ -0,0 +1,30 @@
+package SqrtException;
+import java.util.*;
+public class SqrtException {
+    public static void main(String[] args)throws ArithmeticException{
+        String num = "";
+        int convNum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer >>");
+        num = input.nextLine();
+        try{
+            convNum = Integer.parseInt(num);
+        }
+        catch(Exception e){
+            System.out.println("error");
+            convNum = 0;
+        }
+        try{
+            if(convNum < 0){
+                throw(new ArithmeticException());
+            }
+        }
+        catch(ArithmeticException e){
+            System.out.println("Can't take square root of a negative number...");
+        }
+        
+        System.out.println(Math.sqrt(convNum));
+        
+    }
+    
+}
